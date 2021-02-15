02137ada4db88fa67f284bb70a3485f78b7f7a9e
everton42 <evertonferreira4596@gmail.com>
2021-02-15 11:50:11 -0300

    create

--- src/admin/books/views.py
+++ src/admin/books/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from .models import Book
@@ -12,7 +12,10 @@ class BookViewSet(viewsets.ViewSet):
         return Response(serializer.data)
     
     def create(self, request): #/api/books
-        pass
+        serializer = BookSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None): #/api/books/<str:id>
         pass

50f1e6c1ef001229c3207058dfdb8958264af7c2
everton42 <evertonferreira4596@gmail.com>
2021-02-15 12:17:11 -0300

    retrieve, update, destroy

--- src/admin/books/views.py
+++ src/admin/books/views.py
@@ -18,10 +18,18 @@ class BookViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None): #/api/books/<str:id>
-        pass
+        book = Book.objects.get(id=pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
 
     def update(self, request, pk=None): #/api/books/<str:id>
-        pass
+        book = Book.objects.get(id=pk)
+        serializer = BookSerializer(instance=book, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None): #/api/books/<str:id>
-        pass
+        book = Book.objects.get(id=pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
